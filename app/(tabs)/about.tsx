846f45986711879b1d9a6fa794ec031dbb0db666
rama-27 <ramarajumanten231@gmail.com>
2024-11-24 21:45:49 +0530

    day 1 of react native

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,25 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function AboutScreen(){
+    return (
+    <View style={styles.container}>
+            <Text style={styles.text}>About Screen</Text>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+    container:{
+
+        flex:1,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    text: {
+
+        color:'yellow',
+    },
+})
