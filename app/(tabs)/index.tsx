b086d06ef575d7781babe656e46f4661cc78b15e
rama-27 <ramarajumanten231@gmail.com>
2024-12-01 09:16:47 +0530

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,14 @@
 import { Text, View,  StyleSheet } from 'react-native';
-import {Link } from 'expo-router';
+import { Image } from 'expo-image';
+
+const placeHolderImage =require('@/assets/images/background-image.png')
 
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>
-                Yo screen
-            </Text>
-            <Link href="/about" style={styles.button}>
-                Go to About Section</Link>
-
+            <View style={styles.imageContainer}>
+            <Image source={placeHolderImage} style={styles.image}/>
+            </View>
         </View>
     );
 }
@@ -17,19 +16,17 @@ export default function Index() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'black',
+        backgroundColor: '#25292e',
         alignItems: 'center',
-        justifyContent: 'center',
     },
-    text: {
-        color: '#fff',
+    imageContainer:{
+        flex: 1,
     },
-    button:{
+    image:{
+        width:320,
+        height:440,
+        borderRadius:18,
+    },
 
-        fontSize: 20,
-        textDecorationLine: 'underline',
-        color: 'purple',
-
-    },
 });
 
